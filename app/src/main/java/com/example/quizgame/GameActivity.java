2f7ca3359fd3a0175cd08b133d100489caca8a28
Brian Hong <73767581+brhong8@users.noreply.github.com>
2023-04-29 06:53:19 -0700

    Implemented pseudocode into GameActivity

--- app/src/main/java/com/example/quizgame/GameActivity.java
+++ app/src/main/java/com/example/quizgame/GameActivity.java
@@ -2,82 +2,97 @@ package com.example.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
-    TextView question = (TextView) findViewById(R.id.question);
-    Button buttonA = (Button) findViewById(R.id.buttonA);
-    Button buttonB = (Button) findViewById(R.id.buttonB);
-    Button buttonC = (Button) findViewById(R.id.buttonC);
-    Button buttonD = (Button) findViewById(R.id.buttonD);
+    private final String SERVER_IP = "192.168.0.20";
+    private final int SERVER_PORT = 8000;
+    private boolean gameActive;
+    Thread clientThread;
+    TextView question;
+    Button buttonA;
+    Button buttonB;
+    Button buttonC;
+    Button buttonD;
+    CountDownTimer timer;
+    long timeLeft;
+    String userAnswer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        // TODO: Send request for questions/answers and parse response from server
-        question.setText("This is a placeholder question?");
+        gameActive = true;
 
+        // Start connection to server
+        TCPClient client = new TCPClient();
+        clientThread = new Thread(client);
+        clientThread.start();
 
-        buttonA.setText("A");
+        question = findViewById(R.id.question);
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        buttonD = findViewById(R.id.buttonD);
+
         buttonA.setOnClickListener(this);
-
-
-        buttonB.setText("B");
         buttonB.setOnClickListener(this);
-
-
-        buttonC.setText("C");
         buttonC.setOnClickListener(this);
-
-        buttonD.setText("D");
         buttonD.setOnClickListener(this);
-
-        while (true) {
-            String message = "";
-            while (received == false) { // Wait for JSON from server
-                message = messageFromServer();
-            }
-            JSONObject gs = null;
-            try {
-                gs = new JSONObject(message);
-            } catch (JSONException e) {
-                throw new RuntimeException(e);
-            }
-            if (gs == null) { // If server sends a game end signal
-                break;
-            }
-            setGameState(gs);
-        }
-
-        endGame(); // Function to clean up and end game
 
     }
     @Override
     public void onClick(View v) {
+        JSONObject obj = new JSONObject();
         switch(v.getId()) {
             case R.id.buttonA:
-                sendResponse("A");
+                try {
+                    obj.put("choice", 0);
+                    obj.put("time", timeLeft);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
                 break;
             case R.id.buttonB:
-                sendResponse("B");
+                try {
+                    obj.put("choice", 1);
+                    obj.put("time", timeLeft);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
                 break;
             case R.id.buttonC:
-                sendResponse("C");
+                try {
+                    obj.put("choice", 2);
+                    obj.put("time", timeLeft);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
                 break;
             case R.id.buttonD:
-                sendResponse("D");
+                try {
+                    obj.put("choice", 3);
+                    obj.put("time", timeLeft);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
                 break;
         }
-        // TODO: Send response of answer to server
+        userAnswer = obj.toString();
 
         buttonA.setEnabled(false);
         buttonB.setEnabled(false);
@@ -85,21 +100,95 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         buttonD.setEnabled(false);
     }
 
-    private void setGameState(JSONObject gameState) {
-        question.setText(gameState.get("question"));
-        buttonA.setText(gameState.get("a"));
-        buttonB.setText(gameState.get("b"));
-        buttonC.setText(gameState.get("c"));
-        buttonD.setText(gameState.get("d"));
+    private void setGameState(String jsonString) throws JSONException {
+        JSONObject gameState = new JSONObject(jsonString);
 
+        if (gameState.getBoolean("isLastQuestion")) {
+            gameActive = false;
+        }
+
+        // Set question
+        question.setText(gameState.getString("Question"));
+
+        // Set answers
+        JSONArray answers = gameState.getJSONArray("Options");
+        buttonA.setText(answers.getString(0));
+        buttonB.setText(answers.getString(1));
+        buttonC.setText(answers.getString(2));
+        buttonD.setText(answers.getString(3));
+
+        // Enable buttons
         buttonA.setEnabled(true);
         buttonB.setEnabled(true);
         buttonC.setEnabled(true);
         buttonD.setEnabled(true);
 
+        // Start timer
+        timer = new CountDownTimer(20000, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeft = (l / 1000) % 60;
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
     }
 
-    private void endGame() {
+    class TCPClient implements Runnable {
+        public PrintWriter out;
+        public BufferedReader in;
 
+        String response;
+
+        public void run() {
+            try {
+                // Try to connect to server
+                Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+
+                // Setup input and output for socket connection
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                try {
+                    while (gameActive) {
+                        // Get game state JSON from server
+                        response = receiveJsonData();
+                        setGameState(response);
+                        userAnswer = null;
+                        // Busy wait for user response
+                        while (timeLeft > 0 && userAnswer == null) {
+                        }
+                        if (timeLeft == 0) {
+                            userAnswer = ""; // User didn't answer in time
+                        }
+                        sendJsonData(userAnswer);
+                    }
+                } catch (Exception e) {
+                } finally {
+                    in.close();
+                    out.close();
+                    socket.close();
+                }
+
+            } catch (Exception e) {
+
+            }
+        }
+
+        public String receiveJsonData() throws Exception {
+            StringBuilder response = new StringBuilder();
+            char tmp;
+            while ((tmp = (char)in.read()) != '#') {
+                response.append(tmp);
+            }
+
+            return response.toString();
+        }
+        public void sendJsonData(String json) throws Exception {
+            out.print(json + '#');
+            out.flush();
+        }
     }
 }
--- app/src/main/java/com/example/quizgame/MainActivity.java
+++ app/src/main/java/com/example/quizgame/MainActivity.java
@@ -15,80 +15,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button createRoom = (Button) findViewById(R.id.button);
+        Button createRoom = findViewById(R.id.button);
         createRoom.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.button:
-                Intent createRoom = new Intent(this, GameActivity.class);
-                startActivity(createRoom);
-                break;
-            case R.id.button3:
-                Intent joinRoom = new Intent(this, GameActivity.class);
-                startActivity(joinRoom);
-                break;
+        if (v.getId() == R.id.button) {
+            Intent start = new Intent(this, GameActivity.class);
+            startActivity(start);
         }
     }
 
-    //This function is called when joinRoom is selected
-    public boolean clientConnect(){
-        //Attempt to connect to server
-        //Wait for server to respond
-        //Let player know we are in a room
-        //Wait for server to send game start info
-        //Call clientLoop
-
-        int timeOut = 30;                                           //Time to wait until we give up connecting to server
-        int waitTime = 1;                                           //How long to wait while trying to connect
-        int timer = 0;                                              //Variable to hold how long we've waited
-        String receivedData;                                        //String to store data received from server
-
-        //Create the JSON file we send to the server
-        JsonObject clientConnectJSON = JSon.createObjectBuilder()   
-            .add("client", Json.createObjectBuilder()
-                .add("connectionType", "joinRoom")
-                .add("response", "null"))
-            .build();
-        //sendToServer(clientConnectJSON.toString)                  //Send the JSON to the server as a string
-        System.out.println("Connecting to server\n");             //Let the user know we're trying to connect
-        receivedData = "false";                                     //Set recveivedData to false to start the while loop
-        //Loop until we have either timed out or received data from the server
-        while(receivedData=="false"){                               //If we haven't received any data               
-            receivedData = receiveFromServer();                     //Fetch data from the socket
-            System.out.println(".");
-            TimeUnit.SECONDS.sleep(waitTime);                       //Wait for waitTime 
-            timer = timer + waitTime;                               //Increase how long we've waited for       
-            if (timer > timeOut){                                   //Check to see if we've timed out
-                println("\n[ERROR] Could not connect to server - Timed Out\n");
-                return false;
-            }
-        }
-        println("Connected to server\nWaiting for game to start...\n");
-        receivedData = "false";
-
-        //Loop until we receive data from the server
-        while(receivedData=="false"){                               //If we haven't received any data
-            receivedData = receiveFromServer();                     //Fetch data from the socket
-            TimeUnit.SECONDS.sleep(waitTime);                       //Wait for waitTime
-        }
-        //Convert the data received from the server back to JSON
-        //JSONParser parser = new JSONParser();
-        //JSONObject serverData = (JSONObject) parser.parse(receivedData);
-        try{
-            JsonObject serverData = JSON.parse(receivedData);
-        }catch (Exception e){
-            System.out.println("[ERROR] Malformed server data - Invalid JSON\n");
-        }
-        //Parse the JSON into variables
-        if (serverData["Question"] == "start"){                     //If the server is telling us to start
-            System.out.println("Game is starting...\n");          //Alert the user the game is starting   
-            return true;                                            //Return true (Advance to game state) 
-        }
-        System.out.println("[ERROR] Failed to start game - " + serverData["Question"]);
-        return false;
-
-    }
 }
